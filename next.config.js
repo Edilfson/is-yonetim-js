abc3eae241fce35a8d8485365c7ebf29e6c95bc6
Edilfson <mcaisbeda@gmail.com>
2025-05-14 10:16:33 +0300

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // next.config.js
 module.exports = {
-  output: 'export', // Statik içerik oluşturmak için
   experimental: {
-    appDir: true, // Bu satır, App Directory kullanımını etkinleştirir
+    appDir: true,
   },
+  output: 'export',
 };
